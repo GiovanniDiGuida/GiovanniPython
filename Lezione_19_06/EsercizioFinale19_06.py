d38d5c2dc0c209f332c17c7a7327b472fa993e57
Sedex <giovanni.di.guida95@gmail.com>
2024-06-19 17:44:34 +0200

    Add files via upload

---
+++ Lezione_19_06/EsercizioFinale19_06.py
@@ -0,0 +1,16 @@
+controllo_ciclo1= True
+Inventario=["tavolo","sedia","tovaglia"]
+
+print("\nMenu:")
+print("1: Visualizza Inventario")
+print("2: Acquista prodotto")
+print("3: Esci")
+Selezione=input("\n Cosa vuoi fare?")
+while True:
+    if Selezione==1:
+    print(Inventario)
+    if Selezione==2:
+    
+
+
+
