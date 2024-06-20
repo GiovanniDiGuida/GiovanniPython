4b9c10466e7280d2d087a057404b960ba23bd87e
Sedex <giovanni.di.guida95@gmail.com>
2024-06-20 09:54:08 +0200

    Add files via upload

--- Lezione_20_06/EsercizioCicli.py
+++ Lezione_20_06/EsercizioCicli.py
@@ -1,6 +1,19 @@
 controllo_ciclo1= True
 # Variabili
+Tavolo={"Tavolo1": "legno",
+           "prezzo": 500,
+           "quantità":3}
+Sedia={"Sedia1": "legno",
+       "prezzo": 200,
+       "quantità": 10}
+Tovaglia={"tovaglia1": "cotone",
+          "prezzo": 10,
+          "quantità":50}
+Tavolo=="tavolo"
+Sedia=="sedia"
+Tovaglia=="tovaglia"
 Inventario=["tavolo","sedia","tovaglia"]
+InventarioAcquisti=[]
 # Gestione Clienti
 print("\nMenu:")
 print("1: Visualizza Inventario")
@@ -16,14 +29,15 @@ while True:
         print(Inventario)
         compra=input("scegli oggetto:")
         if compra=="tavolo":
-            Inventario.remove("tavolo")
-            print(Inventario)
+            InventarioAcquisti.insert("tavolo")
+            print(InventarioAcquisti)
         if compra=="sedia":
-            Inventario.remove("sedia")
-            print(Inventario)
+            InventarioAcquisti.insert("sedia")
+            print(InventarioAcquisti)
         if compra=="tovaglia":
-            Inventario.remove("tovaglia")
-            print(Inventario)
+            InventarioAcquisti.insert("tovaglia")
+            print(InventarioAcquisti)
+            
     elif Selezione==3:
         print("Buona giornata, Grazie")
     else:
