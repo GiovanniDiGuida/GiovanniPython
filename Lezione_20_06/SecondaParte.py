7c0620b32cdc975668fbfd919858793c3da49f6c
Sedex <giovanni.di.guida95@gmail.com>
2024-06-20 15:30:43 +0200

    Seconda_parte

---
+++ Lezione_20_06/SecondaParte.py
@@ -0,0 +1,9 @@
+condizione= True
+x=int(input("Scrivi Numero Intero Positivo"))
+while condizione:
+ for i in range(x,-1,-1):
+   print (i) 
+   if x:=0:
+     condizione= False
+else:
+  print("Ciclo Finito")
