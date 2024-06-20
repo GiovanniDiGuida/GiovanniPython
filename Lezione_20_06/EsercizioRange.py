4d4d4ca709328f19c80609b791dab0dc8c7f899b
Sedex <giovanni.di.guida95@gmail.com>
2024-06-20 12:47:23 +0200

    EsercizioRange

---
+++ Lezione_20_06/EsercizioRange.py
@@ -0,0 +1,18 @@
+cond= True
+Numero=int(input("InserisciNumero:"))
+for i in range(Numero,-1,-1):
+    print (i) 
+
+Condizione=input("Vuoi aggiungere un altro numero? (si/no)")
+while cond:
+     Condizione== "si"
+Numero=int(input("InserisciNumero:"))
+for i in range(Numero,-1,-1):
+    print (i) 
+if Condizione=="no":
+    cond= True
+    print("Grazie per l'utilizzo")
+else:
+    ("Errore")
+
+
