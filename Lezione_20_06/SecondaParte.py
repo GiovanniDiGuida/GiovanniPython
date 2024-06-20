123c5e27f428014609c210b961a1925c461a22c9
Sedex <giovanni.di.guida95@gmail.com>
2024-06-20 15:36:40 +0200

    Seconda_Parte

--- Lezione_20_06/SecondaParte.py
+++ Lezione_20_06/SecondaParte.py
@@ -3,7 +3,8 @@ x=int(input("Scrivi Numero Intero Positivo"))
 while condizione:
  for i in range(x,-1,-1):
    print (i) 
-   if x:=0:
-     condizione= False
-else:
+ if x==0:
+  condizione= False
+ else:
   print("Ciclo Finito")
+ break
