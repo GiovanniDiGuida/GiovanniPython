35be163824588175e14f0dcd6ca9baab61945ac8
Sedex <giovanni.di.guida95@gmail.com>
2024-06-20 15:58:30 +0200

    Terza_parte

---
+++ Lezione_20_06/TerzaParte.py
@@ -0,0 +1,12 @@
+cond= True
+while cond:
+    lista=[]
+    x=input("Vuoi aggiungere numeri? si/no")
+    if x=="si":
+        y=int(input("Aggiungi un numero"))
+        y in lista
+    elif x=="no":
+        cond: False
+    for x in lista:
+     print(x*x)
+
