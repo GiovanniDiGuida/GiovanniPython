037e1723289d66521146203897fbc282298f3402
Sedex <giovanni.di.guida95@gmail.com>
2024-06-21 14:26:16 +0200

    Add files via upload

---
+++ Lezione_21_06/EsercitazioneWhile.py
@@ -0,0 +1,15 @@
+cond= True
+
+while cond:
+    ListaNumeri=[]
+    Numeri=int(input("Inserisci numero"))
+    if Numeri>0:
+        ListaNumeri.append(Numeri)
+    elif Numeri==0:
+        sum(Numeri)
+        print(sum)
+        cond= False
+    else:
+        ("errore")
+
+
