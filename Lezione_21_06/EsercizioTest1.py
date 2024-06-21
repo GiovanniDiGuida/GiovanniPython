b4be6d1924ab7d1513352ff57099fdce6556aeb3
Sedex <giovanni.di.guida95@gmail.com>
2024-06-21 10:48:16 +0200

    Add files via upload

---
+++ Lezione_21_06/EsercizioTest1.py
@@ -0,0 +1,22 @@
+cond= True
+while cond:
+    Stringa=input("aggiungi una parola:")
+    if Stringa=="a":
+        print(Stringa)
+        print(len(Stringa))
+    elif Stringa=="e":
+        print(Stringa)
+        print(len(Stringa))
+    elif Stringa=="i":
+        print(Stringa)
+        print(len(Stringa))
+    elif Stringa=="o":
+        print(Stringa)
+        print(len(Stringa))
+    elif Stringa=="u":
+        print(Stringa)
+        print(len(Stringa))
+    else:
+        print("Errore")
+     
+     #credo si debba usare un ciclo while però non so come far capire al programma di cercare le vocali
