8b6b963bf935b0a6539f00a6948769f8152f03d7
Sedex <giovanni.di.guida95@gmail.com>
2024-07-01 17:29:30 +0200

    Add files via upload

---
+++ Lezione_01_07/EsercizioFinale.py
@@ -0,0 +1,47 @@
+class Ristorante:
+
+    stato= False
+    menù={}
+
+    def stato_apertura1(self):
+        if self.stato:
+            print("Ristorante chiuso")
+    def stato_apertura2(self):
+        x=input("Il ristorante è aperto? si/no")
+        if x=="si":
+            self.stato= True
+            print("Ristorante aperto")
+        else:
+            print("Il ristorante è chiuso")
+
+    def __init__(self,nome,tipo_cucina):
+
+        self.nome=nome
+        self.tipo_cucina=tipo_cucina
+    
+    def menù(self):
+        y=input("Vuoi aggiungere piatti? si/no")
+        if y=="si":
+            z=input("Come si chiama il piatto?")
+            c=int(input("Quanto costa?:"))
+            self.menù[z]= c
+    
+    def stamp_menù(self):
+        print(self.menù)
+        
+    
+    
+    
+    
+    def __str__(self):
+        return f"nome: {self.nome}, : {self.tipo_cucina}"
+    
+
+ristorante1= Ristorante("Zia Bella","Casareccia")
+print(ristorante1)
+Ristorante.stato_apertura1()
+Ristorante.stato_apertura2()
+Ristorante.menù()
+Ristorante.stamp_menù()
+
+        
