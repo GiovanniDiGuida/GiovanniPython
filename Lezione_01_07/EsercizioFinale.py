7588f2724e6f2967150f835f75a43f1389f4b074
Sedex <giovanni.di.guida95@gmail.com>
2024-07-02 09:33:35 +0200

    Add files via upload

--- Lezione_01_07/EsercizioFinale.py
+++ Lezione_01_07/EsercizioFinale.py
@@ -3,16 +3,17 @@ class Ristorante:
     stato= False
     menù={}
 
-    def stato_apertura1(self):
-        if self.stato:
-            print("Ristorante chiuso")
+    
     def stato_apertura2(self):
         x=input("Il ristorante è aperto? si/no")
         if x=="si":
             self.stato= True
-            print("Ristorante aperto")
-        else:
+            print("Ristorante aperto: Benvenuto")
+        elif x=="no":
             print("Il ristorante è chiuso")
+            pass
+        else:
+            pass
 
     def __init__(self,nome,tipo_cucina):
 
@@ -20,14 +21,27 @@ class Ristorante:
         self.tipo_cucina=tipo_cucina
     
     def menù_(self):
-        y=input("Vuoi aggiungere piatti? si/no")
-        if y=="si":
-            z=input("Come si chiama il piatto?")
-            c=int(input("Quanto costa?:"))
-            self.menù[z]= c
+        while True:
+            y=input("Vuoi aggiungere piatti? si/no")
+            if y=="si":
+                z=input("Come si chiama il piatto?: ")
+                c=int(input("Quanto costa?:"))
+                self.menù[z]= c
+            elif y=="no":
+                break
+            else:
+                print("Errore digitalizzazione")
     
-    
-    
+    def rimuovi_piatti(self):
+        x=input("Quale piatto vuoi rimuovere?: ")
+        if x.lower() in self.menù:
+            self.menù.pop(x)
+        else:
+            print("Il piatto non è presente nel menù")
+        
+        print(self.menù)
+                
+
     def stamp_menù(self):
         print(self.menù)
         
@@ -36,15 +50,15 @@ class Ristorante:
     
     
     def __str__(self):
-        return f"nome: {self.nome}, : {self.tipo_cucina}"
+        return f"nome: {self.nome}, Tipo cucina: {self.tipo_cucina}"
     
 
 ristorante1= Ristorante("Zia Bella","Casareccia")
 print(ristorante1)
-ristorante1.stato_apertura1()
 ristorante1.stato_apertura2()
 ristorante1.menù_()
 ristorante1.stamp_menù()
+ristorante1.rimuovi_piatti()
 
 
 
